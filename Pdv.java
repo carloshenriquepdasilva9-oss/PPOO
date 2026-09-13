f1f1ed0bedc820821b76b9a4093a0e4bb816cb9d
carloshenriquepdasilva9-oss <carloshenriquepdasilva9@gmail.com>
2026-09-12 22:12:29 -0300

    Add files via upload
    Projeto utilizando Java Swing, com o tema de PDV( ponto de venda), utilizando 3 telas e com 5 ou mais elementos.

---
+++ Pdv.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pdv;
+
+/**
+ *
+ * @author Aura monster
+ */
+public class Pdv {
+
+    public static void main(String[] args) {
+       TelaLogin janela1 = new TelaLogin();
+       
+       
+       janela1.setVisible(true);
+    }
+}
